edacf72e745a0e2db918a1a68104ae12fa7972af
Syswea <873550310@qq.com>
2022-09-13 15:06:56 +0800

    master

--- Template/Dynamic Programming/Multiple_Knapsack.cpp
+++ Template/Dynamic Programming/Multiple_Knapsack.cpp
@@ -1,11 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+const int N = 110;
+
+int v[N], w[N], s[N];
+int dp[N][N];
 int main () {
     int n, m;
     cin >> n >> m;
-    vector<int> v(n), w(n), s(n);
-    int dp[n + 1][m + 1];
-    for (int i = 1; i <= n; i ++ ) cin >> v[i] >> w[i] >> s[i];
+    for (int i = 1; i <= n; i ++ ) {
+        cin >> v[i] >> w[i] >> s[i];
+    }
     for (int i = 1; i <= n; i ++ ) {
         for (int j = 0; j <= m; j ++ ) {
             for (int k = 0; k <= s[i] && k * v[i] <= j; k ++ ) {
