5053e477acde30357800e45c07f5c98ea8ff2bef
Syswea <873550310@qq.com>
2022-09-24 18:50:13 +0800

    master

--- Template/Dynamic Programming/Counting.cpp
+++ Template/Dynamic Programming/Counting.cpp
@@ -8,10 +8,13 @@ int f[N][N];
 
 int main () {
     cin >> n;
+    // f[i][j] 1 ~ i kinds num to j
     f[0][0] = 1;
     for (int i = 1; i <= n; i ++ )
         for (int j = i; j <= n; j ++ )
-            f[i][j] = f[i - 1][j] + f[i][j - i];
-    cout << f[n][n] << endl;
+            f[i][j] = f[i][j] + f[i][j - i];
+    int ans = 0;
+    for (int i = 1; i <= n; i ++ ) ans += f[i][n];
+    cout << ans << endl;
     return 0;
 }
