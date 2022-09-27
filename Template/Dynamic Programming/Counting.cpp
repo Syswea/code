3afe3f7f77424e813e207a9624af556d94dbb6a6
Syswea <873550310@qq.com>
2022-09-27 16:52:56 +0800

    master

--- Template/Dynamic Programming/Counting.cpp
+++ Template/Dynamic Programming/Counting.cpp
@@ -3,18 +3,33 @@ using namespace std;
 
 const int N = 1010, p = 1e9 + 7;
 
-int n;
-int f[N][N];
-
 int main () {
+
+    int n;
+    int f[N];
+
     cin >> n;
-    // f[i][j] 1 ~ i kinds num to j
-    f[0][0] = 1;
+    f[0] = 1;
     for (int i = 1; i <= n; i ++ )
         for (int j = i; j <= n; j ++ )
-            f[i][j] = f[i][j] + f[i][j - i];
-    int ans = 0;
-    for (int i = 1; i <= n; i ++ ) ans += f[i][n];
-    cout << ans << endl;
+            f[j] = (f[j] + f[j - i]) % p;
+    cout << f[n] << endl;
+    return 0;
+}
+
+int main () {
+
+    int n;
+    int f[N][N];
+
+    cin >> n;
+    f[1][1] = 1;
+    for (int i = 2; i <= n; i ++ )
+        for (int j = 1; j <= i; j ++ ) {
+            f[i][j] = (f[i - 1][j - 1] + f[i - j][j]) % p;
+        }
+    int res = 0;
+    for (int i = 1; i <= n; i ++ ) res = (res + f[n][i]) % p;
+    cout << res << endl;
     return 0;
 }
