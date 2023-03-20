e3c38f66894f5e588b92f02ef7facd89fd4c5c73
Syswea <873550310@qq.com>
2023-03-20 08:59:51 +0800

    master

--- Template/Search And Graph Theory/Chromatograma.cpp
+++ Template/Search And Graph Theory/Chromatograma.cpp
@@ -1,40 +1,28 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
 int main () {
-    int n, m;
-    cin >> n >> m;
-    vector<vector<int>> ver(n + 1);
-    for (int i = 1; i <= m; i ++ ) {
-        int a, b;
-        cin >> a >> b;
-        ver[a].push_back(b);
-        ver[b].push_back(a);
+    cin.tie(0) -> sync_with_stdio(0);
+    int n, m; cin >> n >> m;
+    vector<int> e[n + 1];
+    for (int i = 0; i < m; i ++ ) {
+        int u, v; cin >> u >> v;
+        e[u].push_back(v);
+        e[v].push_back(u);
     }
-
-    int flag = 1;
     vector<int> color(n + 1, 0);
-    function<int(int, int)> dfs = [&](int u, int v) {
-        color[u] = v;
-        for (int i = 0; i < (int)ver[u].size(); i ++ ) {
-            int j = ver[u][i];
-            if (color[j] == 0) {
-                if (dfs(j, 3 - v) == 0) return 0;
-            }
-            else if (color[j] == v) return 0;
+    function<int(int, int)> dfs = [&](int u, int c) {
+        color[u] = c;
+        for (int v: e[u]) {
+            if (color[v] == 0 && dfs(v, 3 - c) == 0) return 0;
+            else if (color[v] == c) return 0;
         }
         return 1;
     };
-    for (int i = 1; i <= n; i ++ ) {
-        if (color[i] == 0) {
-            if (dfs(i, 1) == 0) {
-                flag = 0;
-                break;
-            }
+    int f = 1;
+    for (int i = 1; i <= n; i ++ )
+        if (color[i] == 0 && dfs(i, 1) == 0) {
+            f = 0;
+            break;
         }
-    }
-
-    cout << (flag? "Yes": "No") << endl;
-    return 0;
+    cout << (f? "Yes": "No") << '\n' ;
 }
