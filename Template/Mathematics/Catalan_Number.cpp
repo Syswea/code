7f80183fab09986da4bba0e19cafdcc21e07ee5a
Syswea <873550310@qq.com>
2024-05-09 21:02:22 +0800

    master

--- Template/Mathematics/Catalan_Number.cpp
+++ Template/Mathematics/Catalan_Number.cpp
@@ -1,6 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
+#define combin
 signed main () {
+    int n;
+    combin(2 * n, n) / (n + 1);
     return 0;
 }
+/*
+1. 括号匹配问题：给定 n 对括号，求出所有正确匹配的括号组合的数量。
+2. 凸多边形划分问题：将 n 边凸多边形划分成 n - 2 个三角形的方法数。
+3. 栈的出栈序列：给定 n 个元素，求出栈的所有可能出栈序列的数量。
+4. 二叉搜索树的计数问题：给定 n 个节点，求出所有可能的二叉搜索树的数量。
+*/
